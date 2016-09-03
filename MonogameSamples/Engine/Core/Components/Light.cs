462e038ebf95595da0be1647156d7e7da13eaa83
Vladimir <vvkraf@gmail.com>
2016-09-03 14:52:44 +0300

    start implement light system

---
+++ MonogameSamples/Engine/Core/Components/Light.cs
@@ -0,0 +1,21 @@
+﻿using MonogameSamples.Engine.Core.Common;
+using MonogameSamples.Engine.Graphics;
+using System;
+using System.Collections.Generic;
+
+namespace MonogameSamples.Engine.Core.Components
+{
+    public class Light : EntityDrawableComponent
+    {
+        public Light(Entity entity) : base(entity)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+
+            base.Initialize();
+        }
+    }
+}
