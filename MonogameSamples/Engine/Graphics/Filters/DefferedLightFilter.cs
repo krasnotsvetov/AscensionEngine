f9b16e0b01f6631fe4f941ca4c392382311d6cda
Vladimir <vvkraf@gmail.com>
2016-09-11 23:49:28 +0300

    Filters for RenderSystem

---
+++ MonogameSamples/Engine/Graphics/Filters/DefferedLightFilter.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonogameSamples.Engine.Core;
+using MonogameSamples.Engine.Core.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonogameSamples.Engine.Graphics.Filters
+{
+    public class DefferedLightFilter : Filter
+    {
+
+        Effect effect;
+        
+        public DefferedLightFilter(RenderSystem renderSystem) : base(renderSystem)
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            effect = GameInfo.Content.Load<Effect>("shaders\\lightEffect");
+            base.Initialize();
+        }
+
+        public void Render(Texture2D diffuse, Texture2D normalMap, Texture2D lightMap, List<Light> lights) 
+        {
+            spriteBatch.Begin(effect: effect); 
+            effect.Parameters["ScreenWidth"].SetValue(width);
+            effect.Parameters["ScreenHeight"].SetValue(height);
+            effect.Parameters["LightCount"].SetValue(lights.Count);
+            effect.Parameters["NormalMap"].SetValue(normalMap);
+            effect.Parameters["LightMap"].SetValue(lightMap);
+
+            effect.Parameters["positionLight"].SetValue(lights.Select(t => (t.ParentComponent as Entity).GlobalTransform.Position).ToArray());
+            effect.Parameters["colorLight"].SetValue(lights.Select(t => t.LightColor).ToArray());
+            effect.Parameters["invRadiusLight"].SetValue(lights.Select(t => t.InvRadius).ToArray());
+
+            spriteBatch.Draw(diffuse, Vector2.Zero, Color.White);
+            spriteBatch.End();   
+        }
+    }
+}
---
+++ MonogameSamples/Engine/Graphics/Filters/Filter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonogameSamples.Engine.Graphics.Filters
+{
+    public class Filter : IDisposable
+    {
+
+        public RenderSystem RenderSystem { get; }
+        protected SpriteBatch spriteBatch;
+        protected float width;
+        protected float height;
+
+        public Filter(RenderSystem renderStstem)
+        {
+            RenderSystem = renderStstem;
+        }
+
+
+        public virtual void Initialize()
+        {
+            this.width = RenderSystem.Device.Viewport.Width;
+            this.height = RenderSystem.Device.Viewport.Height;
+            spriteBatch = new SpriteBatch(RenderSystem.Device);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+    }
+}
